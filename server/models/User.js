3f2aba32821456f88da0b0f936163eeaae9f342d
manicharan-12 <gade.manicharan12@gmail.com>
2025-01-19 12:32:30 +0530

    Add User Schema

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  accessToken: String,
+  refreshToken: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
