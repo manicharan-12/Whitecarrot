b66be94f9fa6a3cc88b51215a47bb63feaa7ec97
manicharan-12 <gade.manicharan12@gmail.com>
2025-01-19 12:48:21 +0530

    Updated the Main file

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Layout/Navbar";
+import Login from "./components/Auth/Login";
+import CalendarView from "./components/Calendar/CalendarView";
+import PrivateRoute from "./components/Auth/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-100">
+          <Navbar />
+          <div className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <CalendarView />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
