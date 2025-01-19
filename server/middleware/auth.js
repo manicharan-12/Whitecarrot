488de61034cb86af2670d3c6edee500dd2929883
manicharan-12 <gade.manicharan12@gmail.com>
2025-01-19 21:05:30 +0530

    Test log

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,6 @@
 const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
+    console.log(req.user)
     return next();
   }
   res.status(401).json({ error: "Authentication required" });
